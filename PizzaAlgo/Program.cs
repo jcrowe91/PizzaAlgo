ca55ad5ad487640f40838c4f1bce88d7d8cd9d76
jcrowe91 <jcrowe9119@gmail.com>
2022-01-01 15:20:14 -0600

    Add project files.

---
+++ PizzaAlgo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PizzaAlgo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Step 1: Call pizza joint.");
+            Console.WriteLine("Step 2: Order Pizza.");
+            Console.WriteLine("Step 3: Pay for pizza.");
+            Console.WriteLine("Wait for pizza to be delivered.");
+        }
+    }
+}
