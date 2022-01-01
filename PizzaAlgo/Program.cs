697fa19965bec3b7857968b37ab7705cdf4340df
jcrowe91 <jcrowe9119@gmail.com>
2022-01-01 15:24:04 -0600

    Finished exercise

--- PizzaAlgo/Program.cs
+++ PizzaAlgo/Program.cs
@@ -7,7 +7,8 @@ namespace PizzaAlgo
         static void Main(string[] args)
         {
             Console.WriteLine("Step 1: Call pizza joint.");
-            Console.WriteLine("Step 2: Order Pizza.");
+            Console.WriteLine("Ask about current specials.");
+            Console.WriteLine("Step 2: Order pizza.");
             Console.WriteLine("Step 3: Pay for pizza.");
             Console.WriteLine("Wait for pizza to be delivered.");
         }
